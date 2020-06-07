be5246597d5a72a322482943cc55d1a66431db08
Lewis Silletto <lsilletto@caredash.com>
2020-06-07 13:14:16 -0400

    add build configs for local

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Client } from 'boardgame.io/react';
-import { SocketIO } from 'boardgame.io/multiplayer'
 import { Table } from './components/board'
 import {Splendor} from "./Game"
 import logger from 'redux-logger';
 import { applyMiddleware } from 'redux';
+import { Local } from 'boardgame.io/multiplayer'
 
 const SplendorClient = Client({
     game: Splendor,
     board: Table, 
     debug: false, 
-    multiplayer: SocketIO({ server: 'localhost:8000' }),
+    multiplayer: Local(),
     enhancer: applyMiddleware(logger),
 });
 
--- src/index.js
+++ src/index.js
@@ -8,28 +8,36 @@ import {Splendor} from "./Game"
 
 import { Lobby } from 'boardgame.io/react';
 
-// The below will render the lobby, but it makes it tough to debug.
-// TODO: Come up with a better way of running the lobby.
-ReactDOM.render(
-  <React.StrictMode>
-    <Lobby
-      gameServer={`https://${window.location.hostname}:8000`}
-      lobbyServer={`https://${window.location.hostname}:8000`}
-      gameComponents={[{game: Splendor, board: Table}]}
-    />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+const NO_LOBBY = process.env.REACT_APP_NO_LOBBY
 
+// TODO: Need https for prod.
+const SERVER = `https://${window.location.hostname}:8000`
+console.log(process.env)
 
-// Code for local deployment no lobby both players on one screen.
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <SplendorClient playerID="0"/>
-//     <SplendorClient playerID="1"/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+if (!NO_LOBBY) {
+  // Render the lobby. This relies on a running server.
+  // TODO: Make the lobby way nicer looking!
+  ReactDOM.render(
+    <React.StrictMode>
+      <Lobby
+        gameServer={SERVER}
+        lobbyServer={SERVER}
+        gameComponents={[{game: Splendor, board: Table}]}
+      />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+} else {
+  // Code for local deployment no lobby both players on one screen, no seperate server.
+  ReactDOM.render(
+    <React.StrictMode>
+      <SplendorClient playerID="0"/>
+      <SplendorClient playerID="1"/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
